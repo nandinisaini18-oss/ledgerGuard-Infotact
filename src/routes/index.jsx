e26b0cc9bb5d6c392445d1019e47958f3f1a25c7
MohamedIslaam <mohamedislaam87@gmail.com>
2026-08-01 00:45:03 +0530

    updated

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -14,9 +14,6 @@ import CreateTransaction from '../pages/CreateTransaction'
 import EditTransaction from '../pages/EditTransaction'
 import ViewTransaction from '../pages/ViewTransaction'
 import Analytics from '../pages/Analytics'
-import CompanyList from '../pages/CompanyList'
-import CompanyDetails from '../pages/CompanyDetails'
-import EditCompany from '../pages/EditCompany'
 import ProtectedRoute from '../components/ProtectedRoute'
 import AdminRoute from '../components/AdminRoute'
 import NotFound from '../pages/NotFound'
@@ -71,9 +68,6 @@ function AppRoutesInner() {
               <Route path="/transactions/:id" element={<ViewTransaction />} />
               <Route path="/transactions/:id/edit" element={<AdminRoute><EditTransaction /></AdminRoute>} />
               <Route path="/analytics" element={<Analytics />} />
-              <Route path="/companies" element={<AdminRoute><CompanyList /></AdminRoute>} />
-              <Route path="/companies/:id" element={<AdminRoute><CompanyDetails /></AdminRoute>} />
-              <Route path="/companies/:id/edit" element={<AdminRoute><EditCompany /></AdminRoute>} />
             </Route>
           </>
         ) : (
--- src/services/company.js
+++ src/services/company.js
@@ -3,19 +3,3 @@ import api from './api'
 export function registerCompany(data) {
   return api.post('/api/auth/company/register', data)
 }
-
-export function getCompanies() {
-  return api.get('/api/companies')
-}
-
-export function getCompanyById(id) {
-  return api.get(`/api/companies/${id}`)
-}
-
-export function updateCompany(id, data) {
-  return api.put(`/api/companies/${id}`, data)
-}
-
-export function toggleCompanyStatus(id) {
-  return api.patch(`/api/companies/${id}/status`)
-}
